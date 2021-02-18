adb6526f302eab67890ba3b6b0909904325abb47
Crowdbotics <team@crowdbotics.com>
2021-02-18 22:02:17 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/soky_mms_24607/MainActivity.java
@@ -0,0 +1,15 @@
+package com.soky_mms_24607;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "soky_mms_24607";
+    }
+}
